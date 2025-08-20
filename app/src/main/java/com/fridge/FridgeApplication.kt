ed35aa79232cbadb26a0dffedba06c8afc7e4d4d
Jovan.Ristic <jovan.ristic@codeplicity.com>
2025-08-20 14:37:43 +0200

    Added Hilt

---
+++ app/src/main/java/com/fridge/FridgeApplication.kt
@@ -0,0 +1,12 @@
+package com.fridge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FridgeApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
